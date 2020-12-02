95a75e2ee36f5bfaf5aea600e912732354d259bc
hugogomz10 <hugogomz10@gmail.com>
2020-12-02 17:43:02 -0600

    Dependencias

--- index.js
+++ index.js
@@ -11,13 +11,16 @@ const notFound = require('./middleware/notFound');
 const index = require('./middleware/index');
 const cors = require('./middleware/cors');
 
+app.use(cors);
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/", (req, res, next) =>{
-    res.status(200);
-    res.send("Bienvenido");
-});
-
+app.get("/", index);
+app.use("/user", user);
+app.use(auth);
+app.use("/recursos", recursos);
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
