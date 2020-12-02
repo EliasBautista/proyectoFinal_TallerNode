1093d9cfa5604cbfd3e18c5b254915c0ea9b02b9
hugogomz10 <hugogomz10@gmail.com>
2020-12-02 14:23:12 -0600

    Config BD

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: '' //Nombre BD
+    database: 'tallernode'
 });
 
 pool.query = util.promisify(pool.query);
